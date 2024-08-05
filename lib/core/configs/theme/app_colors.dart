1416176e262ed857e8e8de013bbbcd4a8dba6f1c
KamTom <kamil8tomaszewski@gmail.com>
2024-08-05 11:31:04 +0200

    Add color palette

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFFF1C40F);
+  static const primaryLight = Color(0xFFFBF0C3);
+  static const secondary = Color(0xFF1695E8);
+  static const secondaryLight = Color(0xFFB9DFF8);
+
+  static const background = Color(0xFFF4F0EF);
+  static const backgroundDark = Color(0xFFE5E2E0);
+
+  static const text = Color(0xFF201D1B);
+  static const textBackground = Color(0xFF4F4741);
+  static const textLight = Color(0xFF464646); 
+
+  static const divider = Color(0xFFC7C0BE); 
+}
