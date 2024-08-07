af33588c43d9d91f2c003b5670b33337159de327
KamTom <kamil8tomaszewski@gmail.com>
2024-08-07 10:12:48 +0200

    Add AppTheme class with light theme configuration

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hive_note/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
+  );
+}
