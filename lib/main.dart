339fbe3a7f5491075419b23d5debb6e7fe929861
KamTom <kamil8tomaszewski@gmail.com>
2024-08-23 22:12:58 +0200

    feat: initialize language support in main.dart
    - Add easy_localization package.
    - Configure supported locales in `Info.plist` for iOS compatibility.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:hive_note/core/configs/theme/app_theme.dart';
-import 'package:hive_note/navigation/presentation/pages/dashboard.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'core/configs/theme/app_theme.dart';
+import 'navigation/presentation/pages/dashboard.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('pl')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
       home: DashboardPage()
